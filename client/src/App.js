f402c98d19852c0978e5c206f6e21c4b9defe1fa
CodingCCarpenter <christineassists@aol.com>
2019-11-10 21:38:41 -0600

    add components to render function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import logo from './logo.svg';
+import axios from 'axios';
+
+import Players from './components/Players.js';
+import ToggleDarkMode from './components/ToggleDarkMode.js';
+
 import './App.css';
 
 class App extends React.Component {
@@ -16,25 +19,22 @@ class App extends React.Component {
 
   componentDidMount() {
     axios
-      .get('http://localhost:5000/api/players')
+      .get("http://localhost:5000/api/players")
       .then((res) => {
         console.log(res.data);
         this.setState({ players: res.data });
       })
-      .catch((err) => {
-        console.log("OH NO! We have an error :(  Let's see what the error message is!", err))
-      }
-  
- render(){ 
-  return(
-    <div className = 'App'>
-      <h1>Women's World Cup Line-up</h1>
-      <h2>Basic information about all the best athletes!</h2>
-      <Players players={this.state.players} />
-      <ToggleDarkMode />
-    </div>
-  );
-};
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ToggleDarkMode />
+        <Players players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
